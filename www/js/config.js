2d01641d5c8a30676ae7696a6f0e9eb2c7673b66
Naim Jeem <naim36208@gmail.com>
2017-04-16 00:23:56 +0600

    init

---
+++ www/js/config.js
@@ -0,0 +1,6 @@
+if (document.location.hostname == "node.fountaintechies.com" ){
+	var baseURL= "http://node.fountaintechies.com:2001/api/";
+} else {
+	var baseURL= "http://localhost:2001/api/";
+}
+//storeProvider.setStore('sessionStorage');
